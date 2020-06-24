a34b2b4345a278d8e4cc0bef485f55c8ce42d5b6
ibnuprtma <pibnu86@gmail.com>
2020-06-24 22:34:54 +0700

    laravel ui

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::redirect('/admin', '/admin/dashboard', 301);
 require('admin.php');
 require('landing-page.php');
 require('report.php');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
