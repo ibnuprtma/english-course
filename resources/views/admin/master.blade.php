258f709a2fb76b2a88e343ad884b24ed8426006a
ibnuprtma <pibnu86@gmail.com>
2020-06-24 13:46:47 +0700

    aset admin

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <meta name="author" content="Creative Tim">
+  <title>English Course</title>
+  @include('admin.include._header-script')
+</head>
+
+<body>
+  <!-- Sidenav -->
+  @include('admin.include._sidenav')
+
+  <!-- Main content -->
+  <div class="main-content">
+    <!-- Top navbar -->
+    @include('admin.include._navbar')
+    <!-- Header -->
+    <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+      <div class="container-fluid">
+        <div class="header-body">
+          <!-- Content -->
+          @yield('content-head')
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid mt--7">
+        @yield('content-body')
+        @include('admin.include._footer')
+    </div>
+  </div>
+  @include('admin.include._footer-script')
+</body>
+
+</html>
