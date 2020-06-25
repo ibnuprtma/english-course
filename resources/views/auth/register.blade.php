0b64974cbda42e45803867571ba437301f1616ce
ibnuprtma <pibnu86@gmail.com>
2020-06-25 23:57:20 +0700

    redesign view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -74,4 +74,96 @@
         </div>
     </div>
 </div>
+@endsection --}}
+
+@extends('landing-page.master')
+
+@section('content')
+    <!-- bradcam_area  -->
+    <div class="bradcam_area">
+        <div class="bradcam_shap">
+            <img src="{{ asset('assets/seogo/img/ilstrator/bradcam_ils.png')}}" alt="">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="bradcam_text text-center">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col-7">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title p-3">Register</h5>
+                                        <hr>
+                                        <form method="POST" action="{{ route('register') }}">
+                                            @csrf
+
+                                            <div class="form-group row">
+                                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                                    @error('name')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                                    @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                                    @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row mb-0 p-3">
+                                                <div class="col-md-12">
+                                                    <button type="submit" class="btn btn-primary w-50">
+                                                        {{ __('Register') }}
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /bradcam_area  -->
 @endsection
+
