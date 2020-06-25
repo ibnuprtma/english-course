b060898b377b04f5f77b20188067b2e82063c59a
ibnuprtma <pibnu86@gmail.com>
2020-06-25 18:22:20 +0700

    add relation

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $table= 'roles';
+    protected $gruarderd= ['id'];
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function user()
+    {
+        return $this->hasMany(App\User::class);
+    }
 }
