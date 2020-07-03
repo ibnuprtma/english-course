24ec2e7dd15af5736c96ef1024ad7d9745baadc7
ibnuprtma <pibnu86@gmail.com>
2020-07-03 17:39:02 +0700

    send email when register success

--- app/Http/Controllers/LandingPage/RegisterStudentController.php
+++ app/Http/Controllers/LandingPage/RegisterStudentController.php
@@ -7,6 +7,8 @@ use App\Models\Student;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Mail\WelcomeMail;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterStudentController extends Controller
 {
@@ -21,17 +23,40 @@ class RegisterStudentController extends Controller
     {
         $email = $request->email;
         $password = $request->firstname . 123;
+        $payment = "BCA 123123123123 A.N Ibnu Pratama";
+
+        if($request->level == "beginner"){
+            $price = "120.000";
+        }elseif($request->level == "basic"){
+            $price = "420.000";
+        }elseif($request->level == "intermediate"){
+            $price = "620.000";
+        }elseif($request->level == "advanced"){
+            $price = "1.120.000";
+        }
 
         //create and find id
         $lastid = Student::create($request->all())->id;
 
-        $user = User::create([
+        User::create([
             'role_id'      => 2,
             'student_id'  => $lastid,
             'username'     => '',
             'email'        => $email,
             'password'     => Hash::make($password),
         ]);
+
+        $details = [
+            'firstname'=>$request->firstname,
+            'email'=>$request->email,
+            'password'=>$password,
+            'payment'=>$payment,
+            'level'=>$request->level,
+            'price'=>$price
+        ];
+
+        \Mail::to($email)->send(new WelcomeMail($details));
+
         return redirect('/')->with('success', 'Data telah terkirim');
     }
 }
