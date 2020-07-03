b755847bfabb8cb242dcc354bac93ea4144293b2
ibnuprtma <pibnu86@gmail.com>
2020-07-03 17:37:49 +0700

    make mail welcome

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,19 @@
+@component('mail::message')
+Thank Your For Your Trust With Us
+
+Please pay first before your next step <br>
+Your Package is {{ $details['level'] }} Total Rp.{{$details['price']}} <br>
+<b>{{ $details['payment'] }}</b>
+<br>
+note : if you'll completed your payment, lets go to login and upload your payment <br><br>
+For your account login : <br>
+email : {{ $details['email'] }} <br>
+password : {{ $details['password'] }}
+
+@component('mail::button', ['url' => ''])
+Go to Dashboard
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
