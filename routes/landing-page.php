d67619a2500e7bf9fe966aebe857801eda62abd0
ibnuprtma <pibnu86@gmail.com>
2020-07-03 14:28:19 +0700

    change namspace

--- routes/landing-page.php
+++ routes/landing-page.php
@@ -4,13 +4,15 @@
 Route::name('landing-page.')->group(function () {
   Route::group([
 
-    'namespace'     => 'landing-page'
+    'namespace'     => 'LandingPage'
 
   ], function () {
 
     Route::view('/pricing', '/landing-page/pricing/index')->name('pricing');
     Route::view('/about-us', '/landing-page/about-us/index')->name('about-us');
     Route::view('/contact', '/landing-page/contact/index')->name('contact');
+
+    Route::post('/register-student', 'RegisterStudentController@store')->name('register-student');
     
   });
 });
