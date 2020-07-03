9767c9160a128445fb2c8a9475578bccc94df320
ibnuprtma <pibnu86@gmail.com>
2020-07-03 17:37:27 +0700

    change route name

--- routes/landing-page.php
+++ routes/landing-page.php
@@ -8,9 +8,9 @@ Route::name('landing-page.')->group(function () {
 
   ], function () {
 
-    Route::view('/pricing', '/landing-page/pricing/index')->name('pricing');
-    Route::view('/about-us', '/landing-page/about-us/index')->name('about-us');
-    Route::view('/contact', '/landing-page/contact/index')->name('contact');
+    Route::view('/pricing', 'landing-page.pricing.index')->name('pricing');
+    Route::view('/about-us', 'landing-page.about-us.index')->name('about-us');
+    Route::view('/contact', 'landing-page.contact.index')->name('contact');
 
     Route::post('/register-student', 'RegisterStudentController@store')->name('register-student');
     
